6d3e0c288fc91deb766316e9fdf21873df0e64c3
Rahul7560 <rahulramesh125@gmail.com>
2020-07-03 22:38:43 +0530

    first commit

---
+++ submitt.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>submit</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+</head>
+<body class="body1" >
+    <div class="container ss" >
+        <div class="row gg">
+            <div class=" col-sm-4 pict" align="center">
+                <img  class=" "src="https://assets-ouch.icons8.com/free-download/450/e5acb970-8833-4578-877d-0919f758526d.png?filename=bermuda-welcome.png" alt="" srcset="">
+
+            </div>
+            
+            <div class="col-sm-8 frm">
+                <div class="container ">
+                        <h2 class="message mt-5 bolds">Hey dude,</h2>
+                        <h2 class="message" >Please fill the details below , Once i verfied your detail my resume will be sent to your email id. </h2>
+                </div>
+                <div class="container " >
+                    <form class=" sg" action="">
+                        <div class="form-group">
+                          <label class="boldss" for="">Name</label>
+                          <input type="text" class="form-control bg-light" name="" id="" aria-describedby="helpId" placeholder="" required>
+
+                          <label class="boldss mt-2" for="">Email</label>
+                          <input type="text" class="form-control bg-light" name="" id="" aria-describedby="helpId" placeholder="" required>
+                          <label class="boldss mt-2" for="">Contact Number <span class=" tex text-secondary">Optional</span></label>
+                          <input type="text" class="form-control bg-light" name="" id="" aria-describedby="helpId" placeholder="" required>
+                          <label  class="boldss mt-2" for=""> Purpose / Message </label>
+                          <textarea class="form-control bg-light" name="" id="" rows="3"></textarea>
+                          <div class="mt-3 mb-5" align="center" >
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                          </div>
+
+                        </div>
+                       
+
+
+
+
+                </div>
+
+            </div>
+                    
+
+    </div>
+            
+  
+
+    
+</body>
+</html>
